bd98b07599ed2bfb6e1d863e35271f578626d08e
FosterAguilar <113448127+FosterAguilar@users.noreply.github.com>
2022-10-18 08:44:51 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+sprites.onDestroyed(SpriteKind.Projectile, function (sprite) {
+    info.changeScoreBy(1)
+})
+let projectile: Sprite = null
+let mySprite = sprites.create(assets.image`ghosty`, SpriteKind.Player)
+controller.moveSprite(mySprite, 0, 50)
+mySprite.x = 8
+mySprite.setStayInScreen(true)
+info.setScore(0)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . . 6 7 7 7 1 6 . . . . . 
+        . . . . . 6 7 7 7 7 6 . . . . . 
+        . . . . . 6 1 7 7 7 6 . . . . . 
+        . . . . . 6 1 1 7 7 6 . . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, -150, 0)
+    projectile.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
+    if (info.score() < 10) {
+        projectile.vx += 100
+    }
+})
